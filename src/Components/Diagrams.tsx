c8320ced32b0932c0f7c96fa048f7357ff1baa9d
jorgeFabian302 <126297370+jorgeFabian302@users.noreply.github.com>
2024-02-29 19:10:05 -0600

    componente Diagrmas para mostrar grafica primera parte

--- src/Components/Diagrams.tsx
+++ src/Components/Diagrams.tsx
@@ -4,7 +4,7 @@ import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-nati
 export const Diagrams = () => {
   return (
     <TouchableOpacity style={StylesDiagrmas.Container}>
-        <View style={{width:10, height: Dimensions.get('screen').height * 0.34, backgroundColor: 'green'}} />
+        <View style={{width:10, height: Dimensions.get('screen').height * 0.31, backgroundColor: 'green'}} />
 
         <Text>rfrjforufivjoidfvjn</Text>
     </TouchableOpacity>
@@ -16,7 +16,7 @@ export const StylesDiagrmas = StyleSheet.create({
     
     backgroundColor: 'red', 
     marginVertical: 20,
-    height: Dimensions.get('screen').height * 0.34, 
+    height: Dimensions.get('screen').height * 0.31,
     width: Dimensions.get('screen').width * 0.9,     
   },
 })
